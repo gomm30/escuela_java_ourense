7b5a20f3726591bb291581b52e92c0a96daefcbc
Joaquin <joaquin@correo.com>
2019-11-06 15:54:31 +0100

    - Tipos de datos en Java. - EstructurasControl.java con if y bucles - Test unitarios con JUnit para testear métodos de ordenamiento.

---
+++ 01_IntroJava/src/com/vn/introjava/Main.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vn.introjava;
+
+/**
+ * Clase principal del proyecto Ejemplos java
+ *
+ * @author pc
+ */
+public class Main {
+
+    /**
+     * Función que se ejecuta al arrancar el programa
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//        DatosBasicos.mostrarTiposDatos();
+//        EstructurasControl.ejecutarBucles();
+//        EstructurasControl.ejecutarCondiciones();
+//        DatosBasicos.probarOperadores();
+//        Ordenamiento.ordenarArray();
+    }
+}
