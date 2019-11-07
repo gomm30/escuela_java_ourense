6f887e8eddc00bb467d76205c3de1abd0359f6a0
Joaquin <joaquin@correo.com>
2019-11-07 17:09:26 +0100

    - Actualizacion clase Animal

--- 02_Ejer_Herencia_Animales/src/pkg02_ejer_herencia_animales/Animal.java
+++ 02_Ejer_Herencia_Animales/src/pkg02_ejer_herencia_animales/Animal.java
@@ -10,5 +10,49 @@ package pkg02_ejer_herencia_animales;
  * @author pc
  */
 public class Animal {
+    /*Variables*/
+    protected String nombre;	
+    protected int edad;
+    protected int peso;
+    protected Habitat habitat;
+    
+    /*Constructores*/
+    public Animal(){
+        
+    }
+    /*Getters y Setters*/
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+    public Habitat getHabitat() {
+        return habitat;
+    }
+    
+    /*Métodos propios*/
+    
+    
+    
+    
     
 }
