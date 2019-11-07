682f33463bc88e4d3f40840fdbdd3bb21efdea45
Joaquin <joaquin@correo.com>
2019-11-07 16:54:38 +0100

    - Inicio proyecto

---
+++ 02_Ejer_Herencia_Animales/src/pkg02_ejer_herencia_animales/Habitat.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg02_ejer_herencia_animales;
+
+/**
+ *
+ * @author pc
+ */
+public enum Habitat {
+    NO_DEFINIDO,
+    ACUATICO,
+    TERRESTRE,
+    ANFIBIO
+}
