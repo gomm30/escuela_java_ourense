f55e20d958a6bb079959515af1ec8b47e87df989
Joaquin <joaquin@correo.com>
2019-11-25 10:12:53 +0100

    - Tests de UsuarioDaoImp

---
+++ 06_ModeloUsuariosBBDD/src/com/vn/modelousuarios/dao/ServiciosUsuarios.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vn.modelousuarios.dao;
+
+import java.util.List;
+
+/**
+ *
+ * @author pc
+ */
+public class ServiciosUsuarios {
+
+    public void validarDatos() {
+    }
+
+    public Usuario crear(String email, String password, String nombre, int edad) {
+        return null;
+    }
+
+    public Usuario modificar(int Id, String email, String password, String nombre, int edad) {
+        return null;
+    }
+
+    public Usuario modificar(Usuario usuDatosNuevos) {
+        /*Invocar al anterior*/ return null;
+    }
+
+    public boolean eliminar(int id) {
+        return false;
+    }
+
+    public Usuario leerUno(int id) {
+        return null;
+    }
+
+    public Usuario leerUno(String email) {
+        return null;
+    }
+
+    public List<Usuario> leerPorNombre(String nombre) {
+        return null;
+    }
+
+    public List<Usuario> leerTodos() {
+        return null;
+    }
+}
---
+++ 06_ModeloUsuariosBBDD/test/TestUsuarioBBDD.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import com.vn.modelousuarios.dao.ServiciosUsuarios;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author pc
+ */
+public class TestUsuarioBBDD {
+    
+    public TestUsuarioBBDD() {
+    }
+
+     @Test
+     public void testLeerUno() {
+         ServiciosUsuarios su = new ServiciosUsuarios();
+         
+         // Obtener usuario existente por id
+         assertEquals(1,su.leerUno(1).getId());
+         // Obtener usuario existente por id
+         assertNull(su.leerUno(3));
+         
+         // Obtener usuario existente por email
+         assertEquals("usu1@correo.com",su.leerUno("usu1@correo.com").getEmail());
+         // Obtener usuario existente por email
+         assertNull(su.leerUno("correonoexistente"));
+         // Obtener usuario introduciendo email vacio
+         assertNull(su.leerUno(""));
+     }
+}
